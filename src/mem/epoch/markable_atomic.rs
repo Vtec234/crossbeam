525278220006ad8dc720e20ff3344b441503ef50
Wojciech Nawrocki <wjnawrocki+gh@protonmail.com>
2017-01-06 16:42:06 +0000

    Swap and doc

--- src/mem/epoch/markable_atomic.rs
+++ src/mem/epoch/markable_atomic.rs
@@ -5,6 +5,13 @@ use std::sync::atomic::{self, Ordering};
 
 use super::{Owned, Shared, Guard};
 
+/// Like a `(std::sync::atomic::AtomicPtr, bool)` pair.
+///
+/// Provides atomic access to a pair consisting of:
+///
+/// - a (nullable) pointer of type `T`, interfacing with the `Owned` and `Shared` types
+///
+/// - a boolean marker bit
 #[derive(Debug)]
 pub struct MarkableAtomic<T> {
     val: atomic::AtomicUsize,
@@ -40,68 +47,117 @@ fn opt_owned_into_usize<T>(val: Option<Owned<T>>) -> usize {
     ptr as usize
 }
 
+
 impl<T> MarkableAtomic<T> {
+    /// Create a new, null, markable atomic pointer with the given marker value.
     #[cfg(feature = "nightly")]
-    pub const fn null(b: bool) -> MarkableAtomic<T> {
+    pub const fn null(mark: bool) -> MarkableAtomic<T> {
         debug_assert!(mem::align_of::<T>() >= 2);
 
         MarkableAtomic {
-            val: atomic::AtomicUsize::new(tag_val(0, b)),
+            val: atomic::AtomicUsize::new(tag_val(0, mark)),
             _marker: PhantomData,
         }
     }
 
+    /// Create a new, null, markable atomic pointer with the given marker value.
     #[cfg(not(feature = "nightly"))]
-    pub fn null(b: bool) -> MarkableAtomic<T> {
+    pub fn null(mark: bool) -> MarkableAtomic<T> {
         debug_assert!(mem::align_of::<T>() >= 2);
 
         MarkableAtomic {
-            val: atomic::AtomicUsize::new(tag_val(0, b)),
+            val: atomic::AtomicUsize::new(tag_val(0, mark)),
             _marker: PhantomData,
         }
     }
 
-    pub unsafe fn from_ptr(data: *mut T, mark: bool) -> MarkableAtomic<T> {
+    /// Create a new markable atomic pointer.
+    pub fn new(data: T, mark: bool) -> MarkableAtomic<T> {
         debug_assert!(mem::align_of::<T>() >= 2);
 
         MarkableAtomic {
-            val: atomic::AtomicUsize::new(tag_val(data as usize, mark)),
+            val: atomic::AtomicUsize::new(tag_val(Box::into_raw(Box::new(data)) as usize, mark)),
             _marker: PhantomData,
         }
     }
 
-    pub fn new(data: T, b: bool) -> MarkableAtomic<T> {
+    /// Unsafely create a new markable atomic pointer from the given value.
+    pub unsafe fn from_ptr(ptr: *mut T, mark: bool) -> MarkableAtomic<T> {
         debug_assert!(mem::align_of::<T>() >= 2);
 
         MarkableAtomic {
-            val: atomic::AtomicUsize::new(tag_val(Box::into_raw(Box::new(data)) as usize, b)),
+            val: atomic::AtomicUsize::new(tag_val(ptr as usize, mark)),
             _marker: PhantomData,
         }
     }
 
+    /// Do an atomic load with the given memory ordering.
+    ///
+    /// In order to perform the load, we must pass in a borrow of a
+    /// `Guard`. This is a way of guaranteeing that the thread has pinned the
+    /// epoch for the entire lifetime `'a`. In return, you get an optional
+    /// `Shared` pointer back (`None` if the `Atomic` is currently null), with
+    /// lifetime tied to the guard, and the value of the marker bit.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `ord` is `Release` or `AcqRel`.
     pub fn load<'a>(&self, ord: Ordering, _: &'a Guard) -> (Option<Shared<'a, T>>, bool) {
         let p = untag_val(self.val.load(ord));
         (unsafe { Shared::from_raw(p.0 as *mut _) }, p.1)
     }
 
-    pub fn store(&self, val: Option<Owned<T>>, mark: bool, ord: Ordering) {
-        self.val.store(tag_val(opt_owned_into_usize(val), mark), ord);
+    /// Do an atomic store with the given memory ordering.
+    ///
+    /// Transfers ownership of the given `Owned` pointer, if any. Since no
+    /// lifetime information is acquired, no `Guard` value is needed.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `ord` is `Acquire` or `AcqRel`.
+    pub fn store(&self, new: Option<Owned<T>>, mark: bool, ord: Ordering) {
+        self.val.store(tag_val(opt_owned_into_usize(new), mark), ord);
     }
 
-    pub fn store_and_ref<'a>(&self, val: Owned<T>, mark: bool, ord: Ordering, _: &'a Guard)
+    /// Do an atomic store with the given memory ordering, immediately yielding
+    /// a shared reference to the pointer that was stored.
+    ///
+    /// Transfers ownership of the given `Owned` pointer, yielding a `Shared`
+    /// reference to it. Since the reference is valid only for the curent epoch,
+    /// it's lifetime is tied to a `Guard` value.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `ord` is `Acquire` or `AcqRel`.
+    pub fn store_and_ref<'a>(&self, new: Owned<T>, mark: bool, ord: Ordering, _: &'a Guard)
                              -> Shared<'a, T>
     {
         unsafe {
-            let shared = Shared::from_owned(val);
+            let shared = Shared::from_owned(new);
             self.store_shared(Some(shared), mark, ord);
             shared
         }
     }
 
-    pub fn store_shared(&self, val: Option<Shared<T>>, mark: bool, ord: Ordering) {
-        self.val.store(tag_val(opt_shared_into_usize(val), mark), ord);
+    /// Do an atomic store of a `Shared` pointer and a marker bit
+    /// with the given memory ordering.
+    ///
+    /// This operation does not require a guard, because it does not yield any
+    /// new information about the lifetime of a pointer.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `ord` is `Acquire` or `AcqRel`.
+    pub fn store_shared(&self, new: Option<Shared<T>>, mark: bool, ord: Ordering) {
+        self.val.store(tag_val(opt_shared_into_usize(new), mark), ord);
     }
 
+    /// Do a compare-and-set on the pair, from a `Shared` to an `Owned` pointer in the pair
+    /// with the given memory ordering.
+    ///
+    /// As with `store`, this operation does not require a guard; it produces no new
+    /// lifetime information. The `Result` indicates whether the CAS succeeded; if
+    /// not, ownership of the `new` pointer is returned to the caller.
     pub fn cas(&self, old: Option<Shared<T>>, old_mark: bool, new: Option<Owned<T>>, new_mark: bool, ord: Ordering)
                -> Result<(), Option<Owned<T>>>
     {
@@ -116,6 +172,11 @@ impl<T> MarkableAtomic<T> {
         }
     }
 
+    /// Do a compare-and-set on the pair, from a `Shared` to an `Owned` pointer in the pair
+    /// with the given memory ordering, immediatley acquiring a new `Shared` reference to
+    /// the previously-owned pointer if successful.
+    ///
+    /// This operation is analogous to `store_and_ref`.
     pub fn cas_and_ref<'a>(&self, old: Option<Shared<T>>, old_mark: bool, new: Owned<T>, new_mark: bool, ord: Ordering, _: &'a Guard)
                            -> Result<Shared<'a, T>, Owned<T>>
     {
@@ -129,6 +190,10 @@ impl<T> MarkableAtomic<T> {
         }
     }
 
+    /// Do a compare-and-set on the pair, from a `Shared` to another `Shared` pointer in the pair
+    /// with the given memory ordering.
+    ///
+    /// The boolean return value is `true` when the CAS is successful.
     pub fn cas_shared(&self, old: Option<Shared<T>>, old_mark: bool, new: Option<Shared<T>>, new_mark: bool, ord: Ordering)
                       -> bool
     {
@@ -137,11 +202,19 @@ impl<T> MarkableAtomic<T> {
                                   ord) == opt_shared_into_usize(old)
     }
 
-    // .. etc
-    // compare_arc_exchange_mark
-    //swaps..
+    /// Do an atomic swap with an `Owned` pointer in the new pair with the given memory ordering.
+    pub fn swap<'a>(&self, new: Option<Owned<T>>, new_mark: bool, ord: Ordering, _: &'a Guard)
+                    -> (Option<Shared<'a, T>>, bool)
+    {
+        let prev = untag_val(self.val.swap(tag_val(opt_owned_into_usize(new), new_mark), ord));
+        (unsafe { Shared::from_raw(prev.0 as *mut _) }, prev.1 )
+    }
 
-
-
-
+    /// Do an atomic swap with a `Shared` pointer in the new pair with the given memory ordering.
+    pub fn swap_shared<'a>(&self, new: Option<Shared<T>>, new_mark: bool, ord: Ordering, _: &'a Guard)
+                           -> (Option<Shared<'a, T>>, bool)
+    {
+        let prev = untag_val(self.val.swap(tag_val(opt_shared_into_usize(new), new_mark), ord));
+        (unsafe { Shared::from_raw(prev.0 as *mut _) }, prev.1 )
+    }
 }
